c958dc8db3bbedf672eb984759f66065d0eb50b5
Srija S <srijakumar7@gmail.com>
2017-11-22 23:40:07 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,6 @@
-require 'pry'
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  binding.pry
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
